346fce296d0bf161320bb849b958db035d0f9ad9
SebaGPDev <sebakpo120@gmai.com>
2023-08-16 11:47:11 -0300

    add stack navigation and loginScreen complete

--- src/navigation/configureNavigation.js
+++ src/navigation/configureNavigation.js
@@ -1,17 +1,32 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../components/views/HomeScreen";
-import PeopleScreen from './../components/views/PeopleScreen';
+import PeopleScreen from "../components/views/PeopleScreen";
+import LoginScreen from "../components/views/LoginScreen";
+import { useSelector } from "react-redux";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-export default function MyTab() {
+const App = () => {
+  const { ticket } = useSelector((state) => state.auth);
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="People" component={PeopleScreen} />
-      </Tab.Navigator>
+      {!ticket ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      ) : (
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="People" component={PeopleScreen} />
+        </Tab.Navigator>
+      )}
     </NavigationContainer>
   );
-}
+};
+
+export default App;
